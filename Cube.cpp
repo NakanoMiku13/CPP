0763a81e3bdb292d8d5fd9b6eaacfb9eda5b11d7
NakanoMiku13 <danulricht@hotmail.com>
2021-08-18 09:55:26 -0500

    Update Cube Fix some errors

--- Cube.cpp
+++ Cube.cpp
@@ -21,6 +21,7 @@ auto SumTopBottomCol(int x,int y)->int{
     for(int i=0;i<5;i++){
         //In this case we only have to travel in the Z coordinate just for add the top or bottom values
         Sum+=Array[x][y][5-i];
+        Sum+=*(*(*(Array+x)+y)+(5-i));
     }
     return (int)Sum;
 }
