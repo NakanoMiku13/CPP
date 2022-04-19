a31b2655102e89e5e08d4f6745cf575fece15cd9
NakanoMiku13 <danielulrichtamayo@gmail.com>
2022-04-19 00:12:43 -0500

    GCD

---
+++ GCD vs LCM.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+auto main()->int{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n,number;
+    cin>>n;
+    for(auto i=0;i<n;i++){
+        cin>>number;
+        printf("%d %d %d %d\n",number-3,1,1,1);
+    }
+}
