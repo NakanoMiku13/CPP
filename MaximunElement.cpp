5eaac6e241c7a1ed519ee6119aceb4ecf5a484c0
NakanoMiku13 <danielulrichtamayo@gmail.com>
2022-04-19 10:34:30 -0500

    Update

--- MaximunElement.cpp
+++ MaximunElement.cpp
@@ -1,16 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
+stack<int> check,maxis;
 auto main()->int{
     int n,maxi=0;
     cin>>n;
-    for(auto i=0; i<n; i++){
-        int x,y;
-        cin>>x;
-        if(x==1){
-            cin>>y;
-            maxi=max(y,maxi);
-        }else if(x==3){
-            cout<<maxi<<endl;
+    for(auto i=0;i<n;i++){
+        int a;
+        cin>>a;
+        switch(a){
+            case 1:
+                int x;
+                cin>>x;
+                check.push(x);
+                maxi=max(maxi,x);
+            break;
+            case 2:
+                if(check.top()==maxi){
+                    maxi=0;
+                    auto tmp = check;
+                    tmp.pop();
+                    while(!tmp.empty()){
+                        maxi=max(maxi,tmp.top());
+                        tmp.pop();
+                    }
+                }
+                check.pop();
+                break;
+            case 3: cout<<maxi<<"\n";
         }
     }
 }
