f89886f9cb5e93e7e6da0d4426db1049576be7df
NakanoMiku13 <danielulrichtamayo@gmail.com>
2022-06-18 00:49:32 -0500

    Update

---
+++ Curso/4_1AumentoPrefijo.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+auto main()->int{
+    short int shortInt;
+    int numInt;
+    long int longInt;
+    long long longlongInt;
+    unsigned long long int unsignedLongLongInt;
+    float numFloat;
+    double numDouble;
+    long double longDouble;
+    char letter;
+    cout<<"short int "<<sizeof(shortInt)<<endl;
+    cout<<"int "<<sizeof(numInt)<<endl;
+    cout<<"long int "<<sizeof(longInt)<<endl;
+    cout<<"long long int "<<sizeof(longlongInt)<<endl;
+    cout<<"unsigned long long int "<<sizeof(unsignedLongLongInt)<<endl;
+    cout<<"float "<<sizeof(numFloat)<<endl;
+    cout<<"double "<<sizeof(numDouble)<<endl;
+    cout<<"long double "<<sizeof(longDouble)<<endl;
+    cout<<"char "<<sizeof(letter)<<endl;
+}
---
+++ Curso/4_2ConversionPrefijo.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+auto main()->int{
+    int numero = 25;
+    char letra = 'a';
+    cout<<"Esta es una letra "<<letra<<endl;
+    cout<<"Esta es una letra convertida a entero "<<(int)letra<<endl;
+    cout<<"Este es un numero "<<numero<<endl;
+    cout<<"Este es un numero convertido a ascii "<<(char)numero<<endl;
+}
