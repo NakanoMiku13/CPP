0505b055bb3c44b7c5fb2a34adfbe48fd7f1bae8
NakanoMiku13 <danielulrichtamayo@gmail.com>
2022-07-23 01:05:27 -0500

    Update Finishing implementation of the vector

--- Curso/9_13Vector.cpp
+++ Curso/9_13Vector.cpp
@@ -4,43 +4,62 @@ template<typename data>
 using pointer = data*;
 template<typename data>
 struct node{
-    data value;
-    pointer<node> next,prev;
     public:
+        data value;
+        pointer<node> next,prev;
         node(data value): value{value},next{nullptr},prev{nullptr}{}
-    template<typename val>
-    friend ostream& operator<<(ostream&,const pointer<node<val>>&);
+        template<typename val>
+        friend ostream& operator<<(ostream&,const pointer<node<val>>&);
+        auto operator!=(const auto &compare){
+            auto tmp = *this;
+            if(compare->value!=tmp->value) return true;
+            else return false;
+        }
 };
 template<typename dataset>
 ostream& operator<<(ostream& os,const pointer<node<dataset>>& val){
     os<<val->value;
     return os;
 }
+template<typename dataset>
+struct _iterator{
+    private:
+        pointer<node<dataset>> current;
+    public:
+        _iterator(pointer<node<dataset>> currentNode): current(currentNode){}
+        auto &operator++(){
+            current = current->next;
+            return current;
+        }
+        auto operator++(int){
+            auto retVal = *this;
+            ++(*this);
+            return retVal;
+        }
+        auto *operator->()const{
+            return &current->value;
+        }
+        auto &operator*()const{
+            return current->value;
+        }
+        auto operator==(const auto& self){
+            return current == self->current;
+        }
+        auto operator!=(const auto& self){
+            return current != self.current;
+        }
+};
 template<typename data>
 struct Vector{
     private:
+        typedef _iterator<data> iterator;
         pointer<node<data>> _head,_back,_tmp;
         size_t _size;
-        
     public:
         Vector(): _head{nullptr},_back{nullptr},_size{0},_tmp{nullptr}{}
         auto empty()->bool{
             return (_size==0 or not _head or not _back) ? true : false;
         }
-        struct iterator{
-            public:
-                auto operator+(const int index){
-                    _data = _head;
-                    for(auto i=0;i<index;i++,_data=_data->next);
-                    return _data;
-                }
-                auto _begin(){
-                    return _data;
-                }
-            private:
-                pointer<node<data>> _data;
-        };
-        iterator _begin();
         auto push_back(data value){
             auto newNode = new node<data>(value);
             if(empty()) _head = _back = newNode;
@@ -116,15 +135,18 @@ struct Vector{
         auto operator+(const int index){
             return (!empty()) ? retVal(index):NULL;
         }
-        auto operator++(){
-            return _head->next;
+        auto operator!=(const auto &compare){
+            auto tmp = *this;
+            if(compare->value!=tmp->value) return true;
+            else return false;
         }
-        auto begin(){
-            return _head;
+        iterator begin(){
+            return this->_head;
+        }
+        iterator end(){
+            return this->_back->next;
         }
 };
-
-
 auto main()->int{
     int n;
     Vector<int> vectorI;
@@ -145,16 +167,20 @@ auto main()->int{
     }
     cout<<"\nFuncion at: "<<endl;
     for(auto i=0;i<n;i++){
-        cout<<vectorI.at(i)<<endl;
+        cout<<vectorI.at(i)<<" ";
     }
     cout<<"\nAritmetica de apuntadores: "<<endl;
     for(auto i=0;i<vectorI.size();i++){
         auto x = vectorI+i;
-        cout<<x<<endl;
+        cout<<x<<" ";
+    }
+    cout<<"\nForeach: "<<endl;
+    for(auto i : vectorI){
+        cout<<i<<" ";
     }
     cout<<"\nWhile empty: "<<endl;
     while(!vectorI.empty()){
-        cout<<vectorI.front()<<endl;
+        cout<<vectorI.front()<<" ";
         vectorI.pop_front();
     }
 }
--- prueba.cpp
+++ prueba.cpp
@@ -1,90 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define punteroVacio  nullptr
-#define nodoVacio  nullptr
+#define apuntadorVacio nullptr
+#define nodoVacio nullptr
 template<typename dato> using pointer = dato*;
 template<typename informacion> struct nodo{
     public:
         informacion valorAlmacenado;
-        pointer<nodo> siguiente, anterior;
-        //constructor
-        nodo(informacion valorPorAlmacenar): valorAlmacenado(valorPorAlmacenar),
-        siguiente(punteroVacio),anterior(punteroVacio){}
+        pointer<nodo> siguiente;
+        nodo(informacion valorPorAlmacenar): valorAlmacenado(valorPorAlmacenar),siguiente(apuntadorVacio){}
 };
-template<typename tipoDeDato> struct pqueue{
+template<typename dato> struct Pila{
     private:
-        pointer<nodo<tipoDeDato>> _cabeza,_espalda;
-        size_t _cantElementos;
+        pointer<nodo<dato>> _top;
+        size_t _size;
     public:
-        pqueue(): _cabeza(punteroVacio), _espalda(punteroVacio), _cantElementos(0){}
+        Pila(): _top(nodoVacio), _size(0){}
         auto vacia()->bool{
-            return (_cantElementos == 0) ? true : false;
+            return (_size==0 or not _top) ? true : false;
         }
-        auto longitud()->size_t{
-            return _cantElementos;
+        auto push(dato datoRecibido){
+            auto newNode = new nodo<dato>(datoRecibido);
+            if(vacia()) _top = newNode;
+            else{
+                newNode->siguiente = _top;
+                _top = newNode;
+            }
+            _size++;
         }
-        auto quitar(){
+        auto pop(){
             if(vacia()) return;
             else{
-                if(_cabeza == _espalda){
-                    delete _cabeza;
-                    _cabeza = _espalda = punteroVacio;
-                }else{
-                    auto temporal = _cabeza;
-                    _cabeza = _cabeza->siguiente;
-                    delete temporal;
+                if(_top->siguiente == nodoVacio){
+                    delete _top;
+                    _size = 0;
+                    return;
                 }
-                _cantElementos--;
+                auto temporal = _top;
+                _top = _top->siguiente;
+                delete temporal;
+                _size--;
             }
         }
-        auto insertar(tipoDeDato valorNuevo){
-            auto nuevoNodo = new nodo<tipoDeDato>(valorNuevo);
-            if(vacia()) _cabeza = _espalda = nuevoNodo;
-            else{
-                //Caso donde el nuevo elemento es mayor a la _cabeza
-                if(_cabeza->valorAlmacenado < nuevoNodo->valorAlmacenado){
-                    nuevoNodo->siguiente = _cabeza;
-                    _cabeza->anterior = nuevoNodo;
-                    _cabeza = nuevoNodo;
-                }else{
-                    //Caso especial
-                    auto nodoMovimiento = _cabeza;
-                    for(;nodoMovimiento->valorAlmacenado > nuevoNodo->valorAlmacenado
-                    && nodoMovimiento->siguiente != nodoVacio;nodoMovimiento=nodoMovimiento->siguiente);
-                    //Caso donde estamos en el ultimo nodo
-                    if(nodoMovimiento->siguiente == nodoVacio && nodoMovimiento->valorAlmacenado > nuevoNodo ->valorAlmacenado){
-                        _espalda-> siguiente = nuevoNodo;
-                        nuevoNodo->anterior = _espalda;
-                        _espalda = nuevoNodo;
-                    }else{
-                        auto temporal = nodoMovimiento->anterior;
-                        nuevoNodo->siguiente = nodoMovimiento;
-                        nuevoNodo->anterior = temporal;
-                        nodoMovimiento->anterior = nuevoNodo;
-                        temporal->siguiente = nuevoNodo;
-                        _espalda = nodoMovimiento;
-                    }
-                }
-            }
-            _cantElementos++;
+        auto size()->size_t{
+            return _size;
         }
-        auto front(){
-            return _cabeza->valorAlmacenado;
+        auto top()->dato{
+            return _top->valorAlmacenado;
         }
 };
 auto main()->int{
-    pqueue<int> pp;
     int n;
+    Pila<int> pilita;
     cin>>n;
     for(auto i=0;i<n;i++){
         int x;
         cin>>x;
-        pp.insertar(x);
+        pilita.push(x);
     }
-
     cout<<endl;
-    while(!pp.vacia()){
-        cout<<pp.front()<<" ";
-        pp.quitar();
+    system("color a");
+    while(!pilita.vacia()){
+        cout<<pilita.top()<<endl;
+        pilita.pop();
     }
 }
